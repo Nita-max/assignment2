f0bce77815ffa38b29ba3f7119def2bcf3d9f877
NITA WAGHCHAURE <neetawaghchaure97@gmail.com>
2025-08-26 14:32:12 +0530

    Add files via upload

---
+++ great.c
@@ -0,0 +1,28 @@
+void main()
+{
+	int a=10,b=20,c=30;
+	
+	if(a>b)
+	{
+	 if(a>c)
+	 {
+		printf("%d a is greater",a);
+     }
+ 	else
+	 {
+		printf("%d c is greater",c);
+     }
+    }
+	else
+	{
+	if(b>c)
+	 {
+	 	printf("%d b is greater",b);
+	 }
+	 else
+	 {
+	 	printf("%d c is greater",c);
+	 }
+		
+	}
+}
